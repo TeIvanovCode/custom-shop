befaf289eb4d64914ce8a9fcac8163261d41f99f
Teodor <Ivanov>
2019-11-14 22:49:20 +0100

    fix entity not showing in h2
    Added EntityScan annotation in main class so spring can read Entities in project

--- custom-shop/src/main/java/com/sprinter/customShop/customshop/CustomShopApplication.java
+++ custom-shop/src/main/java/com/sprinter/customShop/customshop/CustomShopApplication.java
@@ -2,8 +2,10 @@ package com.sprinter.customShop.customshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan( basePackages = {"com.sprinter.customShop.entity"} )
 public class CustomShopApplication {
 
 	public static void main(String[] args) {
