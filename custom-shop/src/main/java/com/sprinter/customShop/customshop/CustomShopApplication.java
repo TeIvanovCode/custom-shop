d713eda8b09c3c6919e08cb7ad314edba11ac2d8
TeIvanovCode <57773318+TeIvanovCode@users.noreply.github.com>
2019-11-14 21:37:53 +0100

    Add files via upload

---
+++ custom-shop/src/main/java/com/sprinter/customShop/customshop/CustomShopApplication.java
@@ -0,0 +1,13 @@
+package com.sprinter.customShop.customshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomShopApplication.class, args);
+	}
+
+}
---
+++ custom-shop/src/test/java/com/sprinter/customShop/customshop/CustomShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprinter.customShop.customshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
